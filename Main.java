17334b59a227b2d94494a315ab62939f767582e2
zigos88 <96542132+zigos88@users.noreply.github.com>
2021-12-27 19:11:57 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3500;
+        int millesPrice = 20;
+        int milles = ticketPrice / millesPrice;
+        System.out.println(milles + " миль");
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
